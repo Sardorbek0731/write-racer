130f153eaca39974236e4a41c907760060d7df4b
Sardorbek Olimov <sardor.20070731@gmail.com>
2024-11-19 20:13:36 +0500

    added welcome section buttons

--- src/components/welcome/Welcome.tsx
+++ src/components/welcome/Welcome.tsx
@@ -1,15 +1,27 @@
+// CSS
 import "./Welcome.css";
+
+// React Router DOM
+import { NavLink } from "react-router-dom";
 
 function Welcome(): JSX.Element {
   return (
     <section className="welcome">
-      <div className="container">
-        <div className="welcomeTitle wp-100 flex alignCenter column">
-          <h1>
-            Biz bilan yozish tezligingizni oshiring va o'zingizni namoyish
-            qiling
-          </h1>
-          <p>Kompyuteringiz bilan muloqotni yaxshilashni istasangiz biz bilan birga bo'ling.</p>
+      <div className="container welcomeTitle wp-100 flex column alignCenter">
+        <h1>
+          Biz bilan yozish tezligingizni oshiring va o'zingizni namoyish qiling
+        </h1>
+        <p>
+          Kompyuter bilan muloqotni yaxshilashni istasangiz biz bilan bo'ling.
+        </p>
+
+        <div className="welcomeBtns">
+          <NavLink className="welcome_signUp_btn" to="/register">
+            Kirish
+          </NavLink>
+          <NavLink className="race_try_btn" to="/register">
+            Boshlash
+          </NavLink>
         </div>
       </div>
     </section>
